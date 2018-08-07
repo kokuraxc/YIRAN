6f1f9feae5cc1bf7622814648d1d261bb495837d
guo zhengjin <guo.zhengjin@gmail.com>
2018-08-07 16:12:33 +0800

    simple player prefab, spawn bullet, test level

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-    public float movingSpeed = 5f;
-    public float maxSpeed = 5f;
+    float movingSpeed = 10f;
+    float maxSpeed = 1f;
     Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
 	}
 	
@@ -16,10 +19,23 @@ public class PlayerScript : MonoBehaviour {
 	void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        // time and life only move forward
+        moveVertical = moveVertical > 0 ? moveVertical : 0;
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * movingSpeed, ForceMode.Acceleration);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        /**
+        if (!Input.GetMouseButton(0))
+        {
+            rb.AddForce(movement * movingSpeed, ForceMode.VelocityChange);
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+            // Get the mouse delta. This is not in the range -1...1
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            //float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+            transform.Rotate(0, h, 0);
+        }
+    */
     }
 }
---
+++ Assets/Scripts/SpawnBullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBullet : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform spawnTrans;
+    Vector3 direction;
+
+    bool dragging = false;
+    float dragStartTime;
+    Vector2 dragStartLoc;
+    float dragEndTime;
+    Vector2 dragEndLoc;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+            
+
+            
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStartTime = Time.time;
+            dragStartLoc = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            dragEndTime = Time.time;
+            dragEndLoc = Input.mousePosition;
+
+            spawn();
+        }
+	}
+
+    public float factor = 3;
+    void spawn()
+    {
+        float time = dragEndTime - dragStartTime;
+        Vector2 distance = dragEndLoc - dragStartLoc;
+        Vector3 velocity = new Vector3(distance.x, distance.y, 1 / time * 100);
+        velocity = velocity.normalized * factor;
+
+        var bullet = Instantiate<GameObject>(bulletPrefab, spawnTrans);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().AddForce(velocity, ForceMode.VelocityChange);
+    }
+}
